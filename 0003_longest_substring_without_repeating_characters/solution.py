26186fdfe7291c5698ed8bd8fa0eb3fd30fb8bc4
Yang Yang <asceyang@me.com>
2018-12-06 22:26:14 +0900

    added another solution to 0003

--- 0003_longest_substring_without_repeating_characters/solution.py
+++ 0003_longest_substring_without_repeating_characters/solution.py
@@ -23,27 +23,42 @@ class Solution():
             indexByChar[c] = i
         return max(maxLen, len(indexByChar))
 
+class Solution1():
+    def lengthOfLongestSubstring(self, s):
+        i = 0
+        j = 0
+        indexByChar = {}
+        ans = 0
+        while j < len(s):
+            ch = s[j]
+            if ch in indexByChar:
+                ans = max(ans, j - i)
+                i = max(i, indexByChar[ch] + 1)
+            indexByChar[ch] = j
+            j += 1
+        return max(ans, j - i)
+
 def test_empty():
-    assert Solution().lengthOfLongestSubstring(" ") == 1
+    assert Solution1().lengthOfLongestSubstring(" ") == 1
 
 def test_abcabcbb():
-    assert Solution().lengthOfLongestSubstring("abcabcbb") == 3
+    assert Solution1().lengthOfLongestSubstring("abcabcbb") == 3
 
 def test_bbbbb():
-    assert Solution().lengthOfLongestSubstring("bbbbb") == 1
+    assert Solution1().lengthOfLongestSubstring("bbbbb") == 1
 
 def test_pwwkew():
-    assert Solution().lengthOfLongestSubstring("pwwkew") == 3
+    assert Solution1().lengthOfLongestSubstring("pwwkew") == 3
 
 def test_dvdf():
-    assert Solution().lengthOfLongestSubstring("dvdf") == 3
+    assert Solution1().lengthOfLongestSubstring("dvdf") == 3
 
 def test_advdfe():
-    assert Solution().lengthOfLongestSubstring("advdfe") == 4
+    assert Solution1().lengthOfLongestSubstring("advdfe") == 4
 
 def test_dvdf():
-    assert Solution().lengthOfLongestSubstring("dvdf") == 3
+    assert Solution1().lengthOfLongestSubstring("dvdf") == 3
 
 def test_bpfbhmipx():
-    assert Solution().lengthOfLongestSubstring("bpfbhmipx") == 7
+    assert Solution1().lengthOfLongestSubstring("bpfbhmipx") == 7
 
